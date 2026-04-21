4f5adc366388fb5919e75e4fb8ce9510d4aa31df
juliansantamaria0 <santamariajulian856@gmail.com>
2026-04-21 11:20:09 -0500

    feat: :sparkles: se realizo la migracion de RefundStatus

---
+++ Migrations/20260421161923_RefundStatus.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Sistema_de_gestion_de_tiquetes_Aereos.Migrations
+{
+    /// <inheritdoc />
+    public partial class RefundStatus : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "refund_status",
+                columns: table => new
+                {
+                    refund_status_id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    name = table.Column<string>(type: "varchar(50)", maxLength: 50, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_refund_status", x => x.refund_status_id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "uq_refund_status_name",
+                table: "refund_status",
+                column: "name",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "refund_status");
+        }
+    }
+}
